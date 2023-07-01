cd876d5ad155acd0ea222e6b6dfb4421125ebc48
gongshuai <923554473@qq.com>
2023-07-01 15:14:45 +0800

    初始化

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private String name;
+
+    private Integer age;
+
+    private Integer score;
+}
---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @SneakyThrows
+    public void test(){
+
+    }
+
+}
